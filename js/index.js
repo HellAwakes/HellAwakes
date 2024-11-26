d366b4b771365da7bee778e199bc8a956881bb78
Hell awakes <HellAwakesStudio10@gmail.com>
2024-11-26 19:34:38 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 const sidebar = document.getElementById("Sidebar");
 const openButton = document.getElementById("open");
 
-sidebar.style.transform = "translateX(-100%)";
-sidebar.style.transition = "transform 0.3s ease";
-
-
 openButton.addEventListener("click", () => {
-    if (sidebar.style.transform === "translateX(-100%)") {
-        sidebar.style.transform = "translateX(0)";
-    } else {
-        sidebar.style.transform = "translateX(-100%)";
-    }
+    const isClosed = sidebar.style.transform === "translateX(-100%)";
+    sidebar.style.transform = isClosed ? "translateX(0)" : "translateX(-100%)";
 });
