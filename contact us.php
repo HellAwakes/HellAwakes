8d26ee242e82148e0e7dc4201c9aa65d9d9aa5a7
Hell awakes <HellAwakesStudio10@gmail.com>
2024-12-15 20:31:11 +0100

    th

---
+++ contact us.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+    <link rel="stylesheet" href="css/contact us.css">
+</head>
+<body>
+    <div class="Contactform">
+    <label for="fname">Enter your first name here:</label>
+    <input type="text" id="fname">
+    <br>
+    <label for="Lname">Enter your last name here:</label>
+    <input type="text" id="Lname">
+    <br>
+    <label for="gmail"> Put your Gmail here:</label>
+     <input type="email" name="gmail" id="gmail">
+     <br>
+     <hr>
+     <label for="Comment"> Message </label>
+     <textarea name="Comment" id="Comment"></textarea>
+   </div>
+</body>
+</html>
