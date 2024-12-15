4c728120e293f02d95acf912314a95431e36cfeb
Hell awakes <HellAwakesStudio10@gmail.com>
2024-12-15 20:42:46 +0100

    ed

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="Login">
+    <label for="fname">Enter your username here:</label>
+    <input type="text" id="fname">
+    <br>
+    <label for="password">Enter your password here:</label>
+    <input type="text" id="password">
+
+</body>
+</html>
