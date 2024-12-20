736ed77a3aa52a4fe4e803e5a5b9bb1db1f20c98
Hell awakes <HellAwakesStudio10@gmail.com>
2024-12-21 00:11:31 +0100

    contact

--- js/global.js
+++ js/global.js
@@ -7,7 +7,7 @@ function loadPreferences() {
     document.body.style.backgroundColor = savedColor3;
 
 
-    const textElements = document.querySelectorAll("body, h2, p, button"); // Add more elements if needed
+    const textElements = document.querySelectorAll("body, h2, p,"); // Add more elements if needed
     textElements.forEach((element) => {
         element.style.fontSize = savedFontSize + "px";
     });
